47951a2c53ddf7b9dabb09e94c6277431c409b99
felipe baloneker <felipebaloneker23@gmail.com>
2022-04-14 23:42:30 -0300

    updated

--- src/components/NewOrder/index.tsx
+++ src/components/NewOrder/index.tsx
@@ -14,106 +14,114 @@ function NewOrder(){
     return(
         <div className="new_order">
             <div className="order_header">
-                <h2>Qual é a necessidade da sua Empresa:</h2>
+                <h2>Qual é a necessidade da sua Empresa:<span>(selecione)</span></h2>
             </div>
             <div className="order_main">
                 {/* Formulario de requerimento */}
                 <form className="formulario" onSubmit={createNewOrder}>
                     {/* Categoria do pedido */}
-                    <div className="categoria">
-                        <input
-                            type="checkbox"
-                            id="branding"
-                            name="categoria"
-                            value="branding"
-                            onChange={event => setCategory(event.target.value)}
-                        />
-                        <label className="labelForm" htmlFor="branding">
-                            <span>Branding:</span> Sua empresa terá uma renovação de Identidade visual da marca desde a criação  de um nome, Logo, Website, Banner e todos os serviços de design que a empresa precisa para uma nova fase.
-                        </label>
-                        <br />
-                        <input
-                            type="checkbox"
-                            id="logo"
-                            name="categoria"
-                            value="criação de logo"
-                            onChange={event => setCategory(event.target.value)}
-                        />
-                        <label className="labelForm" htmlFor="logo">
-                            <span>Criação de Logo Marca:</span> Elaboramos um Logo unindo figuras estratégicas que fazem relação com sua empresa.
-                        </label>
-                        <br />
-                        <input
-                            type="checkbox"
-                            id="embalagem"
-                            name="categoria"
-                            value="design de embalagem"
-                            onChange={event => setCategory(event.target.value)}
-                        />
-                        <label className="labelForm" htmlFor="embalagem">
-                           <span> Design de Embalagens:</span> Fazemos o design dos seus produtos.
-                        </label>
-                        <br />
-                        <input
-                            type="checkbox"
-                            id="card"
-                            name="categoria"
-                            value="criação de card"
-                            onChange={event => setCategory(event.target.value)}
-                        />
-                        <label className="labelForm" htmlFor="card">
-                            <span>Criação de Card:</span>  Criamos ilustrações para divulgar eventos e produtos da sua empresa
-                        </label>
-                        <br />
-                        <input
-                            type="checkbox"
-                            id="banner"
-                            name="categoria"
-                            value="criação de banner"
-                            onChange={event => setCategory(event.target.value)}
-                        />
-                        <label className="labelForm" htmlFor="banner">
-                            <span>Criação de Banner:</span>  Elaboramos banners e cartazes para divulgar produtos da sua empresa
-                        </label>
-                        <br />
-                        <input
-                            type="checkbox"
-                            id="website"
-                            name="categoria"
-                            value="criação de website"
-                            onChange={event => setCategory(event.target.value)}
-                        />
-                        <label className="labelForm" htmlFor="website">
-                            <span>Criação de Ux Design / WebDesign:</span> Criamos um site único de diretamente voltado ao publico e a divulgação da sua empresa e produtos.
-                        </label>
-                        <br />
-                        <input
-                            type="checkbox"
-                            id="rede-social"
-                            name="categoria"
-                            value="gerenciamento de rede social"
-                            onChange={event => setCategory(event.target.value)}
-                        />
-                        <label className="labelForm" htmlFor="rede-social">
-                            <span>Gerenciamento de Rede Social:</span> Com objetivo de gerar reconhecimento para para sua empresa
-                        </label>
-                        <br />
+                    <div className="categoria_wrp">
+                        <div className="input_wrp">
+                            <input
+                                type="radio"
+                                id="branding"
+                                name="categoria"
+                                value="branding"
+                                onChange={event => setCategory(event.target.value)}
+                            />
+                            <label className="labelForm" htmlFor="branding">
+                                <span>Branding:</span><br></br> Sua empresa terá uma renovação de Identidade visual da marca desde a criação  de um nome, Logo, Website, Banner e todos os serviços de design que a empresa precisa para uma nova fase.
+                            </label>
+                        </div>
+                        <div className="input_wrp">
+                            <input
+                                type="radio"
+                                id="logo"
+                                name="categoria"
+                                value="criação de logo"
+                                onChange={event => setCategory(event.target.value)}
+                            />
+                            <label className="labelForm" htmlFor="logo">
+                                <span>Criação de Logo Marca:</span><br></br> Elaboramos um Logo unindo figuras estratégicas que fazem relação com sua empresa.
+                            </label>
+                        </div>
+                        <div className="input_wrp">
+                            <input
+                                type="radio"
+                                id="embalagem"
+                                name="categoria"
+                                value="design de embalagem"
+                                onChange={event => setCategory(event.target.value)}
+                            />
+                            <label className="labelForm" htmlFor="embalagem">
+                            <span> Design de Embalagens:</span><br></br> Fazemos o design dos seus produtos.
+                            </label>
+                        </div>
+                        <div className="input_wrp">
+                            <input
+                                type="radio"
+                                id="card"
+                                name="categoria"
+                                value="criação de card"
+                                onChange={event => setCategory(event.target.value)}
+                            />
+                            <label className="labelForm" htmlFor="card">
+                                <span>Criação de Card:</span><br></br>  Criamos ilustrações para divulgar eventos e produtos da sua empresa
+                            </label>
+                        </div>
+                        <div className="input_wrp">
+                            <input
+                                type="radio"
+                                id="banner"
+                                name="categoria"
+                                value="criação de banner"
+                                onChange={event => setCategory(event.target.value)}
+                            />
+                            <label className="labelForm" htmlFor="banner">
+                                <span>Criação de Banner:</span><br></br>  Elaboramos banners e cartazes para divulgar produtos da sua empresa
+                            </label>
+                        </div>
+                        <div className="input_wrp">
+                            <input
+                                type="radio"
+                                id="website"
+                                name="categoria"
+                                value="criação de website"
+                                onChange={event => setCategory(event.target.value)}
+                            />
+                            <label className="labelForm" htmlFor="website">
+                                <span>Criação de Ux Design / WebDesign:</span><br></br> Criamos um site único de diretamente voltado ao publico e a divulgação da sua empresa e produtos.
+                            </label>
+                        </div>
+                        <div className="input_wrp">
+                            <input
+                                type="radio"
+                                id="rede-social"
+                                name="categoria"
+                                value="gerenciamento de rede social"
+                                onChange={event => setCategory(event.target.value)}
+                            />
+                            <label className="labelForm" htmlFor="rede-social">
+                                <span>Gerenciamento de Rede Social:</span><br></br> Com objetivo de gerar reconhecimento para para sua empresa
+                            </label>
+                        </div>
                     </div>
                         {/* Detalhes do pedido */}
                     <div className="details">
                             <label htmlFor="assunto">Decreva ainda melhor a sua necessidade:</label>
                             <br />
-                            <input
-                            type="text"
-                            id="assunto"
-                            className="text-input"
-                            placeholder="Descreva melhor"
+                            <textarea
+                            name=""
+                            className="form-textarea"
+                            cols={30}
+                            rows={10}
+                            maxLength={1280}
+                            placeholder="Descreva seu pedido"
                             onChange={event => setDetails(event.target.value)}
-                            required
-                            />
+                            ></textarea>
                     </div>
                     <div className="flex">
-                    <button type="submit">Realizar pedido</button>
+                    <button type="submit" className="submit">Realizar pedido</button>
                   </div>
                 </form>
             </div>
--- src/components/UserHeader/index.tsx
+++ src/components/UserHeader/index.tsx
@@ -1,30 +1,41 @@
+import { useState } from 'react'
 import './styles.scss'
 const avatar = require('../../assets/avatar.png')
 
 function UserHeader(){
-    const name = 'Felipe Baloneker'
+    const [name,] = 'Felipe Baloneker'.split(' ')
+    const [open,setOpen]= useState(false)
+    const setOpenMenu = ()=>{
+        if(open === true){
+            setOpen(false)
+        }
+        else{setOpen(true)}
+    }
+    console.log(open)
+
     return(
+<>
         <header>
             <div className="header_wrp">
             <div className="logo">
-                    <a href="/"><h3 className="logo_header">Win<span>Design</span></h3></a>
+                    <a><h3 className="logo_header">Win<span>Design</span></h3></a>
                 </div>
                 <div className="avatar">
-                    <div className="user_avatar">
+                    <button className="user_avatar" onClick={setOpenMenu}>
                         <img src={avatar} alt="" />
-                    </div>
-                    <div className="user_name">
-                        <p>{name}</p>
-                    </div>
+                    </button>
                 </div>
-                <nav className='menu' >
-                    <ul
-                    >
-                        <li><a href="/login" className='log' >Sair</a></li>
-                    </ul>
-                </nav>
             </div>
         </header>
+        <div className="user_name"
+        style={{display: open ? "flex":"none"}}
+        >
+            <div className="user_menu">
+                <a>{name}</a>
+                <a href="/login" className='log' >Sair</a>
+            </div>
+        </div>
+</>
     )
 }
 export default UserHeader
